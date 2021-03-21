5206c36f98a70a8f2bda51ab46ce3fb4a55f1811
leandrososa <52506861+leandrososa@users.noreply.github.com>
2021-03-21 19:36:54 -0300

    true ip

--- server.js
+++ server.js
@@ -26,8 +26,13 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/whoami", (req,res) => {
-  console.log(req.hostname);
-  res.json({ ipaddress: req.ip, language:req.headers["accept-language"], software: req.headers["user-agent"]});
+  var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+  console.log(ip);
+  res.json({ ipaddress: ip, language:req.headers["accept-language"], software: req.headers["user-agent"]});
 })
 
 // listen for requests :)
